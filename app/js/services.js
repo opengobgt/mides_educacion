fe3c8be48dbb01ebf71c12a07c59d73da7dc5124
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-10-26 03:25:37 -0400

    Update host

--- app/js/services.js
+++ app/js/services.js
@@ -2,44 +2,45 @@
 
 /* Services */
 
+var host = null;
+if (window.location.host.match('localhost'))
+  host = 'localhost:3000';
+else
+  host = 'api.mides.gob.gt';
 
 // Demonstrate how to register services
 angular.module('SAE.services', ['ngResource'])
   .factory('Student', function($resource){
-    //return $resource('http://api.mides.gob.gt/v1/educacion/estudiantes.json', {});
-    return $resource('http://localhost:3000/v1/educacion/estudiantes.json', {});
+    return $resource('http://' + host + '/v1/educacion/estudiantes.json', {});
 })
 
   .factory('School', function($resource){
-    //return $resource('http://api.mides.gob.gt/v1/educacion/school.json', {});
-    return $resource('http://localhost:3000/v1/educacion/escuelas.json', {});
+    return $resource('http://' + host + '/v1/educacion/escuelas.json', {});
 })
 
   .factory('Department', function($resource){
-  //return $resource('http://api.mides.gob.gt/v1/educacion/departamentos.json', {});
-  return $resource('http://localhost:3000/v1/departamentos.json', {});
+  return $resource('http://' + host + '/v1/departamentos.json', {});
 })
 
   .factory('Town', function($resource){
-  //return $resource('http://api.mides.gob.gt/v1/departamentos/:id/municipios.json', {});
-  return $resource('http://localhost:3000/v1/departamentos/:id/municipios.json', {}, {
-    schools: {method: 'GET', url: 'http://localhost:3000/v1/departamentos/:id/municipios/:town_id/escuelas.json', isArray: true}});
+  return $resource('http://' + host + '/v1/departamentos/:id/municipios.json', {}, {
+    schools: {method: 'GET', url: 'http://' + host + '/v1/departamentos/:id/municipios/:town_id/escuelas.json', isArray: true}});
 })
 
   .factory('Sesion', function($resource){
-    return $resource('http://localhost:3000/v1/sesion', {}, {
+    return $resource('http://' + host + '/v1/sesion', {}, {
         iniciar: {method: 'POST'}
     });
 })
 
   .factory('Asistencia', function($resource) {
-    return $resource('http://localhost:3000/v1/educacion/estudiantes/:estudiante_id/asistencias', {}, {
-      directSave: {url: 'http://localhost:3000/v1/educacion/asistencias', method: 'POST', isArray: true}
+    return $resource('http://' + host + '/v1/educacion/estudiantes/:estudiante_id/asistencias', {}, {
+      directSave: {url: 'http://' + host + '/v1/educacion/asistencias', method: 'POST', isArray: true}
     });
   })
 
   .factory('StudentsBySchool', function($resource) {
-    return $resource('http://localhost:3000/v1/educacion/escuelas/:escuela_id/estudiantes.json', {});
+    return $resource('http://' + host + '/v1/educacion/escuelas/:escuela_id/estudiantes.json', {});
   })
 
   .factory('User', function() {
