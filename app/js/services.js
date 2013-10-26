50c665bded527f043fb6f1d2036912bf3841ba8c
Javier Alvarez <jalvarezsamayoa@gmail.com>
2013-10-25 18:55:46 -0600

    login assets

--- app/js/services.js
+++ app/js/services.js
@@ -13,4 +13,7 @@ angular.module('myApp.services', ['ngResource'])
   .factory('School', function($resource){
     //return $resource('http://api.mides.gob.gt/v1/educacion/estudiantes.json', {});
     return $resource('http://localhost:3000/v1/educacion/escuelas.json', {});
+}).value('version', '0.1')
+  .factory('Sesion', function($resource){      
+    return { $resource('http://localhost:3000/v1/educacion/escuelas.json', {}); }
 }).value('version', '0.1');
