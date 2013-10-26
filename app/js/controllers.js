16c87aec2451b0ffc9a7b8acf0a6be6f2ff4ed1d
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-10-26 15:25:55 -0400

    Fix checkbox

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -110,8 +110,13 @@ angular.module('SAE.controllers', [])
     });
   });
 
-  $scope.selectStudent = function() {
-    this.student.checked = this.student.checked ? false : true;
+  $scope.selectStudent = function(student, $event) {
+    student.checked = student.checked ? false : true;
+
+    if ($event.stopPropagation) $event.stopPropagation();
+    if ($event.preventDefault) $event.preventDefault();
+    $event.cancelBubble = true;
+    $event.returnValue = false;
   };
 
   $scope.submitAttendance = function() {
