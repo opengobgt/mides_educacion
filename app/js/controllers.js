de696a3b6d07f1bce14ba8d56f214eac855b1fc7
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-10-25 23:22:56 -0400

    Forgot to un-stash

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,20 +24,20 @@ angular.module('SAE.controllers', []).
     $scope.department = '';
     $scope.town = '';
     $scope.school = '';
-    
+
     $scope.step = 'department';
-    
+
     Department.query(function(response){
       $scope.data.departments = response;
     });
-    
+
     $scope.towns = function(){
       $scope.step = 'town';
       Town.query({id: $scope.department.id}, function(response){
         $scope.data.towns = response;
       });
     }
-    
+
     $scope.schools = function(){
       $scope.step = 'school';
       Town.schools({id: $scope.department.id, town_id: $scope.town.id}, function(response){
@@ -51,10 +51,10 @@ angular.module('SAE.controllers', []).
     $scope.password = '';
     $scope.failed = false;
     $scope.login = function(){
-        Sesion.iniciar( {email: $scope.email, password: $scope.password}, function(response){          
+        Sesion.iniciar( {email: $scope.email, password: $scope.password}, function(response){
             $scope.data.usuario = response;
             $scope.failed = false;
-            
+
             User.isLoggedIn = true;
             User.id = response.id
             User.nombre = response.nombre;
@@ -69,12 +69,17 @@ angular.module('SAE.controllers', []).
         })
     }
   }])
-  .controller('AttendanceCtrl', ['$scope', 'StudentsBySchool', function($scope, StudentsBySchool) {
-     var current_user = { id: 5, schools: ['16-03-0025-43']};
+  .controller('AttendanceCtrl', ['$scope', 'User', 'StudentsBySchool', function($scope, User, StudentsBySchool) {
+     var current_user = null;
+     if (User.isLoggedIn)
+      var current_user = User;
+     else
+      var current_user = { id: 5, escuelas: ['16-03-0025-43']};
      // Make request for Teacher which will get associated school and students
      var students = [];
-     angular.forEach(current_user.schools, function(escuela_id) {
-       students[escuela_id] = StudentsBySchool.query({escuela_id: escuela_id})
+     angular.forEach(current_user.escuelas, function(escuela_id) {
+       students.push(StudentsBySchool.query({escuela_id: escuela_id}));
      })
+     console.log(students);
      $scope.students = students[0];
   }]);
