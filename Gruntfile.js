c08b4834e6dab6a6ca360acae671fec83bd26077
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-10-26 07:20:12 -0400

    Adding useful grunt tasks, production assets, some filters, cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,23 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    watch: {
+      scripts: {
+        files: ['app/lib/angular/angular.min.js', 'app/lib/angular/angular-route.min.js', 'app/lib/angular/angular-resource.min.js', 'app/js/app.js', 'app/js/filters.js', 'app/js/directives.js', 'app/js/services.js', 'app/js/controllers.js', 'app/js/jquery.1.10.2.min.js', 'app/css/pure-min.css', 'app/css/app.css', 'app/css/login.css'],
+        tasks: ['uglify', 'cssmin']
+      }
+    },
     uglify: {
       my_target: {
         files: {
           'app/js/production.js': ['app/lib/angular/angular.min.js', 'app/lib/angular/angular-route.min.js', 'app/lib/angular/angular-resource.min.js', 'app/js/app.js', 'app/js/filters.js', 'app/js/directives.js', 'app/js/services.js', 'app/js/controllers.js', 'app/js/jquery.1.10.2.min.js'],
-          'app/js/test.js': ['app/js/services.js']
         }
       }
     },
     cssmin: {
       combine: {
         files: {
-          'app/css/production.css': ['app/css/app.css', 'app/css/login.css']
+          'app/css/production.css': ['app/css/pure-min.css', 'app/css/app.css', 'app/css/login.css']
         }
       }
     }
@@ -22,6 +27,7 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify', 'cssmin']);
--- app/js/filters.js
+++ app/js/filters.js
@@ -8,4 +8,10 @@ angular.module('SAE.filters', [])
   return function(text) {
     return String(text).replace(/\%VERSION\%/mg, version);
   }
-}]);
+}])
+
+.filter('capitalize', function() {
+  return function(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+});
